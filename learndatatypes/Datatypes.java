00e3925884dc05bb76e3e7a396289a4b4a2eb6b9
prathikshasasikumar6-stack <prathikshasasikumar6@gmail.com>
2025-12-04 14:58:52 +0530

    Add files via upload
    datatypes

---
+++ learndatatypes/Datatypes.java
@@ -0,0 +1,15 @@
+package learndatatypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int a=10;
+		String name="prathi";
+		char intial='s';
+		System.out.println(a);
+		System.out.println(name);
+		System.out.println(intial);
+
+	}
+
+}
